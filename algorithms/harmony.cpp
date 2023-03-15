832e7c8265103410f0eca9adeee5e1f04045eb7e
Raunak Redkar <raunak.redkar@gmail.com>
2023-03-15 22:17:27 +0100

    finished draft of harmony alg

--- algorithms/harmony.cpp
+++ algorithms/harmony.cpp
@@ -2,45 +2,59 @@
  
 using namespace std;
  
-#define faster ios_base::sync_with_stdio(0); cin.tie(0);
 #define sq(x) (x)*(x)
 #define all(x) (x).begin(), (x).end()
 #define F first
 #define S second
-
 
 random_device rd;
 mt19937 rng(rd());
 template<typename T> inline T randint(T lo, T hi) { return uniform_int_distribution<T>(lo, hi)(rng); } // [lo,hi]
 template<typename T> inline T randel(vector<T>& v) { return v[uniform_int_distribution<T>((int)0, v.size() - 1)(rng)]; } // [lo,hi]
 
-int t = 0;
-const int HMS = 20;
+int HMS;
 int ITEM_COUNT;
 int maxWeight;
-vector<vector<bool>> harmonies;
+int best = 0;
+int bestProfit = 0;
+int worst;
+int t = 0;
+const int ITERATIONS = 20;
+const double HMCR_MAX = 0.75, HMCR_MIN = 0.25;
+const double PAR_MAX = 0.75, PAR_MIN = 0.25;
+priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> WORST;
+
+vector<pair<vector<bool>, int>> harmonies;
 vector<pair<int, int>> items;
 vector<pair<double, int>> profitDensity;
 
 double HMCR() {
     // initialize value for parameter HMCR in terms of t
-    double mx = HMCR_MAX - ((HMCR_MAX-HMCR_MIN)/ITERATIONS)*t;
+    double mx = HMCR_MAX - (double)((double)((HMCR_MAX-HMCR_MIN)*t)/ITERATIONS);
     return mx;
 }
 
 double PAR() {
     // initialize value for parameter PAR in terms of t
-    double mx = PAR_MAX - ((PAR_MAX-PAR_MIN)/ITERATIONS)*t;
+    double mx = PAR_MAX - (double)((double)((PAR_MAX-PAR_MIN)*t)/ITERATIONS);
     return mx;
 }
 
 void bernoulli() {
     for(int i = 0; i < HMS; i++) {
+        int profit = 0;
         for(int j = 0; j < ITEM_COUNT; j++) {
             // randomly choose 0 or 1 as decision for j'th item in harmony
             int decision = rand()%2;
-            harmonies[i].push_back((decision ? 1 : 0));
+            harmonies[i].F.push_back((decision ? 1 : 0));
+            if (decision == 1) profit += items[j].F;
         }    
+        harmonies[i].S = profit;
+        if (profit > bestProfit) {
+            bestProfit = profit;
+            best = i;
+        }
+        WORST.push(make_pair(profit, i));
     }
 }
 
@@ -81,32 +95,62 @@ void REPAIR(vector<bool> &CUR) {
     }
 }
 
+void EVALUATE(vector<bool> &CUR, int totalProfit) {
+    if (totalProfit >= harmonies[best].S) {
+        harmonies[best].F = CUR;
+    }
+    else if (totalProfit >= harmonies[worst].S) {
+        harmonies[worst].F = CUR;
+        WORST.pop();
+        WORST.push(make_pair(totalProfit, worst));
+    }
+}
+
 void gen() {
+    double hmcr = HMCR();
+    double par = PAR();
+
     vector<bool> CUR(ITEM_COUNT);
+    int totalProfit = 0;
     for(int i = 0; i < ITEM_COUNT; i++) {
         // TODO: change rng()%2 to double type random for parameter comparisons
-        if (rng()%2 <= HMCR()) {
+        if (rng()%2 <= hmcr) {
             // choose the corresponding bit in best harmony
-            CUR[i] = harmonies[best][i];
+            CUR[i] = harmonies[best].F[i];
         }
         else {
             // choose a harmony x != best -> CUR[i] = x[i];
             int choice = rng()%HMS;
             while(choice == best) choice = rng()%HMS;
             // pitch adjustment
-            if (rng()%2 <= PAR()) {
+            if (rng()%2 <= par) {
                 // flip the bit
                 CUR[i] = (CUR[i] ? 0 : 1);
             }
         }
+        if (CUR[i]) totalProfit += items[i].F;
     }
+
     REPAIR(CUR);
-    EVALUATE(CUR);
+    EVALUATE(CUR, totalProfit);
 }
 
+void DGHS() {
+    cin >> HMS;
+    bernoulli();
+    while(t <= ITERATIONS) {
+        // TODO: calculate best and worst 
+        worst = WORST.top().S;
+        gen();
+        t += 1;
+    }
+    // output the bestProfit
+    cout << bestProfit;
+}
 
 int main() {
-    faster
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
     harmonies.resize(HMS); 
     cin >> ITEM_COUNT >> maxWeight;
     for(int i = 0; i < ITEM_COUNT; i++) {
