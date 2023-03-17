5fcc622d9bfd37295312b5e5c631714c6352706f
Raunak Redkar <68784294+raunakCode@users.noreply.github.com>
2023-03-18 00:17:01 +0100

    bug fix: profit density reversed during ADD phase

--- algorithms/harmony.cpp
+++ algorithms/harmony.cpp
@@ -1,4 +1,4 @@
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
  
 using namespace std;
  
@@ -73,6 +73,7 @@ void REPAIR(vector<bool> &CUR) {
                 }
             }
         }
+        reverse(all(profitDensity));
     }
 }
 
