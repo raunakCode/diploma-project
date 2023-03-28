3005e3d3fa68c20aed195a1d84dfe19265af2938
raunakRedkar <raunak.redkar@gmail.com>
2023-03-28 23:41:19 +0200

    fixed bug: bestProfit not changing after HM initialization

--- algorithms/harmony.cpp
+++ algorithms/harmony.cpp
@@ -1,4 +1,4 @@
-#include <bits/stdc++.h>
+#include "bits/stdc++.h"
  
 using namespace std;
  
@@ -39,7 +39,8 @@ double PAR() {
     double mx = PAR_MAX - (double)((double)((PAR_MAX-PAR_MIN)*t)/ITERATIONS);
     return mx;
 }
-void REPAIR(vector<bool> &CUR) {
+
+int REPAIR(vector<bool> &CUR, int totalProfit) {
     // calculate total weight of harmony vector
     int weight = 0;
     for(int i = 0; i < ITEM_COUNT; i++) {
@@ -53,6 +54,7 @@ void REPAIR(vector<bool> &CUR) {
             }
             CUR[profitDensity[j].S] = 0;
             weight -= items[profitDensity[j].S].S;
+            totalProfit -= items[profitDensity[j].S].F;
             if (weight <= maxWeight) {
                 // terminate DROP phase
                 break;
@@ -70,27 +72,25 @@ void REPAIR(vector<bool> &CUR) {
                 if (potentialWeight <= maxWeight) {
                     weight = potentialWeight;
                     CUR[profitDensity[j].S] = 1;
+                    totalProfit += items[profitDensity[j].S].F;
                 }
             }
         }
         reverse(all(profitDensity));
     }
+    return totalProfit;
 }
 
 void bernoulli() {
     for(int i = 0; i < HMS; i++) {
+        int profit = 0;
         for(int j = 0; j < ITEM_COUNT; j++) {
             // randomly choose 0 or 1 as decision for j'th item in harmony
             int decision = rng()%2;
             harmonies[i].F.push_back((decision ? 1 : 0));
+            if (decision == 1) profit += items[j].F; 
         }    
-        REPAIR(harmonies[i].F);
-        int profit = 0;
-        for(int j = 0; j < ITEM_COUNT; j++) {
-            if (harmonies[i].F[j] == 1) {
-                profit += items[j].F;
-            }
-        }
+        profit = REPAIR(harmonies[i].F, profit);
         harmonies[i].S = profit;
         if (profit > bestProfit) {
             bestProfit = profit;
@@ -100,15 +100,16 @@ void bernoulli() {
     }
 }
 
-
 void EVALUATE(vector<bool> &CUR, int totalProfit) {
     if (totalProfit >= harmonies[best].S) {
         // if CUR harmony is better than best harmony, replace worst with CUR 
         harmonies[best].F = CUR;
+        harmonies[best].S = totalProfit;
     }
     else if (totalProfit >= harmonies[worst].S) {
         // if CUR harmony is better than worst harmony, replace worst with CUR 
         harmonies[worst].F = CUR;
+        harmonies[worst].S = totalProfit;
         WORST.pop();
         // switch out worst with CUR harmony in priority queue
         WORST.push(make_pair(totalProfit, worst));
@@ -142,7 +143,7 @@ void gen() {
         if (CUR[i]) totalProfit += items[i].F;
     }
 
-    REPAIR(CUR);
+    totalProfit = REPAIR(CUR, totalProfit);
     EVALUATE(CUR, totalProfit);
 }
 
@@ -154,7 +155,7 @@ void DGHS() {
         t += 1;
     }
     // output the bestProfit
-    cout << bestProfit;
+    cout << harmonies[best].S; 
 }
 
 int main() {
@@ -172,4 +173,4 @@ int main() {
     }
     sort(all(profitDensity));
     DGHS();
-}   
+} 
