c8025a95072cd44ff93e8210ecb9767a33fa9557
Raunak Redkar <68784294+raunakCode@users.noreply.github.com>
2023-04-09 13:34:42 +0200

    Update DP_bruteforce.cpp

--- algorithms/DP_bruteforce.cpp
+++ algorithms/DP_bruteforce.cpp
@@ -1,45 +1,36 @@
 #include "bits/stdc++.h"
  
-#define el "\n"
-#define faster ios_base::sync_with_stdio(false); cin.tie(NULL);
-#define sq(x) (x)*(x)
-#define pb push_back
-#define mp make_pair
-typedef long long ll;
-typedef long double ld;
-#define all(x) (x).begin(), (x).end()
-#define rep(i, a, b) for (int i = (a); i < (b); ++i)
-#define trav(a, x) for (auto &a : x)
- 
 using namespace std;
  
 void solve() {
     int iterations; cin >> iterations;
-    int n, k;
-    cin >> n >> k;
+    int n, w;
+    cin >> n >> w;
     int cost[n];
     int profit[n];
     for(int i = 1; i <= n; i++) {
         cin >> profit[i] >> cost[i];
     }
-    vector<vector<int>> DP(n+1, vector<int>(k+1));
-    rep(i, 0, n+1) {
-        DP[i][0] = 0;
-        DP[0][i] = 0;
+    vector<vector<int>> table(n+1, vector<int>(w+1));
+    for(int i = 0; i <= n; i++) {
+        table[i][0] = 0;
+    }
+    for(int i = 0; i <= w; i++) {
+        table[0][i] = 0;
     }
     for(int i = 1; i <= n; i++) {
-        for(int j = 1; j <= k; j++) {
-            DP[i][j] = DP[i-1][j];
+        for(int j = 1; j <= w; j++) {
+            table[i][j] = table[i-1][j];
             int left = j-cost[i];
             if (left >= 0) {
-                DP[i][j] = max(DP[i][j], DP[i-1][left] + profit[i]);
+                table[i][j] = max(table[i][j], table[i-1][left] + profit[i]);
             }
         }
     }
-    cout << DP[n][k]; 
+    cout << table[n][w]; 
 }   
  
 int main() {  
-    faster
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
     solve();
 }
